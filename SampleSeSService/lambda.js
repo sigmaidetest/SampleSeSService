0b39d110851063a0b7223fe6d057401915cf8ecb
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-03-09 12:26:57 +0530

    Initial Commit

---
+++ SampleSeSService/lambda.js
@@ -0,0 +1,28 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+exports.handler = function (event, context, callback) {
+	ses.sendEmail({
+		Destination: {
+			ToAddresses: ['randika@adroitlogic.com'],
+			CcAddresses: ['randikanavagamuwa@gmail.com'],
+			BccAddresses: []
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: 'Sample SES Service'
+				}
+			},
+			Subject: {
+				Data: '[Delete]Sample SES Service'
+			}
+		},
+		Source: 'randika@adroitlogic.com',
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
+
+
+	callback(null, 'Successfully executed');
+}
